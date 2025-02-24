dd1717a224821f8ea47daaba04972fec8f4e9e8e
Vedang <vedang.wappnet@gmail.com>
2025-02-24 15:01:27 +0530

    added delted & read routes

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const initNotificationController = require("../controllers/notificationController");
+const { authenticateUser } = require("../middleware/authMiddleware");
 
 module.exports = (io) => {
     const notificationController = initNotificationController(io);
 
     router.post("/", notificationController.sendNotification);
-    router.get("/:id", notificationController.getUserNotifications);
+    router.get("/", authenticateUser, notificationController.getUserNotifications);
+    router.put("/", authenticateUser, notificationController.readNotifications);
+    router.delete("/", authenticateUser, notificationController.deleteNotifications);
 
     return router;
 };
